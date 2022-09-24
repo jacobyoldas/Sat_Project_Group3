30f6ca2c7c696ba602a6aa19c35918981e529389
jacobyoldas <yasinyoldas6666@gmail.com>
2022-09-24 13:13:32 -0400

    Updated Login

--- src/test/java/POM/Login_Page_Elements.java
+++ src/test/java/POM/Login_Page_Elements.java
@@ -19,9 +19,6 @@ public class Login_Page_Elements extends _01_Base_POM {
   @FindBy(css = "button[aria-label='LOGIN']")
   private WebElement loginButton;
 
-  @FindBy(xpath = "//button[text()='Accept all cookies']")
-  private WebElement acceptCookiesButton;
-
   public WebElement getUsernameInput() {
     return usernameInput;
   }
@@ -37,6 +34,10 @@ public class Login_Page_Elements extends _01_Base_POM {
   public WebElement getAcceptCookiesButton() {
     return acceptCookiesButton;
   }
+
+  @FindBy(xpath = "//button[text()='Accept all cookies']")
+  private WebElement acceptCookiesButton;
+
 
 //  private String username = "richfield.edu";
 //
